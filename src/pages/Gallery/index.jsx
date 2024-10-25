139ae643d334e0cb2947e7e23ba9fa37b15d8c6c
illionne <126209567+illionne@users.noreply.github.com>
2024-10-25 19:11:49 +0800

    Nambah fungsi edit dan dekete

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"; //to import the Hooks//
-import { getData, sendData } from "../../utils/api"; //to call the gallery datas from API//
-import { Card, List, Divider, Input, Skeleton, Drawer, FloatButton, Form, Button, notification, Typography } from "antd"; //to import the card, list, and divider from antd design//
+import { getData, sendData, deleteData } from "../../utils/api"; //to call the gallery datas from API//
+import { Card, List, Divider, Input, Skeleton, Drawer, FloatButton, Form, Button, notification, Typography, Popconfirm } from "antd"; //to import the card, list, and divider from antd design//
 import {
   EditOutlined,
   DeleteOutlined,
@@ -12,7 +12,6 @@ import { formatDateIndonesia, ellipsisGenerator } from "../../utils/ui"; //to ge
 const { Text } = Typography;
 
 const Gallery = () => {
-  
   const [api, contextHolder] = notification.useNotification();
   const [form] = Form.useForm();
   const [dataSource, setDataSource] = useState([]);
@@ -20,16 +19,12 @@ const Gallery = () => {
   const [searchText, setSearchText] = useState("");
   const [isDrawer, setIsDrawer] = useState(false);
 
+  const [isEdit, setIsEdit] = useState(false)
+  const [idSelected, setIdSelected] = useState(null)
+
   useEffect(() => {
     getDataGallery();
   }, []);
-
-  const showAlert = (status, title, description) => {
-    api[status] ({
-      message: title,
-      description: description,
-    });
-  };
 
   const getDataGallery = () => {
     setIsLoading(true)
@@ -63,72 +58,132 @@ const Gallery = () => {
   };
 
   const onCloseDrawer = () => {
+    if (isEdit) {
+      setIsEdit(false)
+      form.resetFields()
+    }
     setIsDrawer(false);
   };
+
+  const handleDrawerEdit = (record) => {
+    setIsDrawer(true)
+    setIsEdit(true)
+    setIdSelected(record?.id)
+    form.setFieldValue("name_natures", record?.name_natures);
+    form.setFieldValue("description", record?.description);
+  }
 
   const handleFormSubmit = () => {
     let nameOfNatures = form.getFieldValue("name_natures");
     let descriptionOfNatures = form.getFieldValue("description");
 
-
+    //prepare
     let formData = new FormData()
     formData.append("name_natures", nameOfNatures)
     formData.append("description", descriptionOfNatures)
 
+    let request = !isEdit ? sendData("/api/natures", formData) : sendData("/api/natures/" + idSelected, formData);
 
-    sendData("/api/natures", formData).then(resp => {
-      if(resp?.message === "OK") {
-        showAlert("success", "Success", "Data Sent");
+    //sending data
+    request.then(resp => {
+      if(resp?.message === "OK"){
+        setIsEdit(false);
+        showAlert("success", "success", "Data Edited");
+        setIdSelected(null);
+        showAlert("success", "Success", "Data send");
         form.resetFields() //make the form field empty
         setIsDrawer(false)
         getDataGallery()
-      } else {
-        showAlert("error", "Failed to send data", "Cant send data");
-      } 
-      }).catch(err => {
-      showAlert("error", "Failded to Send data", err.toString());
-   })
-  };
   
+      }else{
+        showAlert("error", "Failed to send data", "Cant send data");
+      }
+    }).catch(err=> {
+      setIsEdit(false);
+      setIdSelected(null);
+      showAlert("error", "Failed to end data", err.toString());
+    })
+
+    
+  };
+
+  const confirmDelete = (record_id) => {
+    let url =`/api/natures/${record_id}`;
+    let params = new URLSearchParams();
+    params.append("id", record_id);
+
+
+    deleteData(url, params)
+    .then((resp) => {
+     
+      if(resp?.status === 200) {
+        showAlert("success", "Data deleted", "Data berhasil terhapus");
+        getDataGallery();
+        form.resetFields();
+        getDataGallery();
+        onCloseDrawer();
+      } else {
+        showAlert("error", "Failed", "Data gagal terhapus");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      showAlert("error", "Failed", "Data gagal terhapus");
+    });
+  };
 
   const drawerSection = () => {
     return (
-        <Drawer title="Basic Drawer" onClose={onCloseDrawer} open={isDrawer} 
-      extra={<Button type="primary" htmlType="submit" onClick={() => handleFormSubmit()}>
-        Submit
+      <Drawer 
+      title="Basic Drawer" 
+      onClose={onCloseDrawer} open={isDrawer} 
+      extra={<Button 
+      htmlType="submit" 
+      style={{background : isEdit ? "green" : "blue", color: "white"}}
+      onClick={() => handleFormSubmit()}>
+
+        {isEdit ? "Apply" : "Submit"}
+      
+
       </Button>}>
         <Form layout="vertical" name="natures_form" form={form}>
           <Form.Item name="name_natures" placeholder="Name of Natures" label="Name of Natures" required>
             <Input placeholder="eg. Mountain"/>
           </Form.Item>
           <Form.Item name="description" placeholder="Description of Natures" label="Description of Natures" required>
-            <Input.TextArea rows={4} placeholder="eg. Mountain" />
+            <Input.TextArea rows={4} placeholder="e.g Mountain"/>
           </Form.Item>
         </Form>
       </Drawer>
-      
-    );
+    )
   }
 
+  const showAlert = (status, title, description) => {
+    api[status]({
+      message:title,
+      description: description,
+    });
+  };
 
   return (
     <div className="p-4 w-full">
       {contextHolder}
       <h2 className="text-2xl font-bold">Nature's Gallery</h2> 
-      <p>List of Gallery</p>
+      <p>This is the page of Gallery of the Natures.</p>
 
-      
+      {/* <Card bordered={false} className="circlebox h-full w-full"> */}
         <FloatButton
-        type="primary"
-        tooltip={<div> Add gallery </div>}
-        icon={<PlusCircleOutlined />}
-        onClick={() => handleDrawer()}
+          type="primary"
+          tooltip={ <div>Add Gallery</div>}
+          icon={<PlusCircleOutlined/>} 
+          onClick={() => handleDrawer()}
         />
         {drawerSection()}
-            
+      {/* </Card>
+       */}
       <Input
       prefix={<SearchOutlined/>}
-      placeholder="Search"
+      placeholder="input search text"
       className="header-search"
       allowClear
       size="large"
@@ -153,25 +208,37 @@ const Gallery = () => {
             <Card
               cover={<img src={item.url_photo} alt={item.name_natures} />} //change
               actions={[
-                <EditOutlined key="edit" />,
-                <DeleteOutlined key="delete" />,
+                <EditOutlined 
+                  key="edit"
+                  onClick={() => handleDrawerEdit(item)}
+                />,
+              <Popconfirm
+                key={item?.id}
+                title="Delete the gallery"
+                description="Are you sure to delete this gallery?"
+                onConfirm={() => confirmDelete(item?.id)}
+                okText="Yes"
+                cancelText="No"
+              >
+                 <DeleteOutlined key="delete" />
+                </Popconfirm>,
               ]}
             >
+
+               
               <Card.Meta
                 avatar={<CheckCircleOutlined />}
-                title={item.name_natures}
+                title={item.name_natures} //change
+                // description={`Posted: ${formatDateIndonesia(
+                  // item?.created_at
                 description={
-                  <Text 
+                  <Text
                     ellipsis={ellipsisGenerator(item?.description)}
                   >
                     {item?.description}
-
                   </Text>
                 }
-                  
               
-                  
-          
                 // )}`}
               />
             </Card>
